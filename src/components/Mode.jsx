0042e4f632bef3b7dbe22a1d1028861912f583b6
recepclkkl <recep.celikkol@hotmail.com>
2025-05-27 17:59:59 +0300

    proje oluşturuldu

---
+++ src/components/Mode.jsx
@@ -0,0 +1,16 @@
+import '../assets/css/Mode.css'
+
+function Mode() {
+  
+  return (
+    <>
+      <div class="flex items-left">
+        <p>Dark Mode</p>
+        <p>|</p>
+        <p>TÜRKÇE' YE GEÇ</p>
+      </div>
+    </>
+  )
+}
+
+export default Mode
