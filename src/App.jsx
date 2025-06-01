15c7e86145d7b553358364ba91267e4503bbd432
recepclkkl <recep.celikkol@hotmail.com>
2025-06-01 15:01:07 +0300

    eksik dosya giderildi

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { lang } from './languange/lang';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import Skills from './components/Skills';
-import Profile from './components/profile';
+import Profile from './components/Profile';
 import Projects from './components/Projects';
 import Footer from './components/Footer';
 import { LanguageProvider } from './context/LanguageContext';
